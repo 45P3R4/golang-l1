4420564bc62546a8b16bbe1298c4d82921b4d848
45P3R4 <langleybaka@gmail.com>
2025-07-13 21:30:13 +0300

    add solution variants

--- l1.13/main.go
+++ l1.13/main.go
@@ -3,12 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	firstNum := 1
-	secondNum := 2
+	ac, bc := 1, 2
 
-	fmt.Println(firstNum, secondNum)
+	//Best solution
+	ac, bc = bc, ac
+	fmt.Println(ac, bc)
 
-	firstNum, secondNum = secondNum, firstNum
+	//XOR (only for integer)
+	ax, bx := 1, 2
+	ax = ax ^ bx
+	bx = ax ^ bx
+	ax = ax ^ bx
+	fmt.Println(ax, bx)
 
-	fmt.Println(firstNum, secondNum)
+	//Sum/sub
+	as, bs := 1, 2
+	as = as + bs
+	bs = as - bs
+	as = as - bs
+	fmt.Println(as, bs)
+
+	//Multiply/divide (not for b=0)
+	am, bm := 1, 2
+	am = am * bm
+	bm = am / bm
+	am = am / bm
+	fmt.Println(am, bm)
 }
