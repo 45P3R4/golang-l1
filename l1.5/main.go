b09f3968726313bbfe291166cd016bc09ed0ecc1
45P3R4 <langleybaka@gmail.com>
2025-07-13 21:43:25 +0300

    l1.5

---
+++ l1.5/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	const runTimeS = 10
+	const tickRateMs = 500
+
+	dataCh := make(chan int, 5)
+
+	timeout := time.After(runTimeS * time.Second)
+	ticker := time.NewTicker(tickRateMs * time.Millisecond)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-timeout:
+			fmt.Println("Exit")
+			return
+
+		case <-ticker.C:
+			num := rand.Intn(100)
+			select {
+			case dataCh <- num:
+				fmt.Println("Send:", num)
+			default:
+				fmt.Println("Channel blocked")
+			}
+
+		case received := <-dataCh:
+			fmt.Println("Received:", received)
+		}
+	}
+}
